d7b876649a53c2e1f74a121e9ce3d69e6efd630f
AnahiAramayo <vanesa.aramayo@gmail.com>
2017-12-12 17:56:36 -0300

    [21] Animaciones: animaciones en el splash, logo reescalado

--- app/src/main/java/com/hgp_android/coleliga/Splash.java
+++ app/src/main/java/com/hgp_android/coleliga/Splash.java
@@ -3,16 +3,30 @@ package com.hgp_android.coleliga;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 /**
  * Created by AnahiAramayo on 29/11/2017.
  */
 
 public class Splash extends AppCompatActivity {
+    LinearLayout l1, l2;
+    Animation arriba_abajo, abajo_arriba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        l1= (LinearLayout) findViewById(R.id.l1);
+        l2= (LinearLayout) findViewById(R.id.l2);
+
+        arriba_abajo = AnimationUtils.loadAnimation(this,R.anim.arriba_abajo);
+        l1.setAnimation(arriba_abajo);
+        abajo_arriba = AnimationUtils.loadAnimation(this,R.anim.abajo_arriba);
+        l2.setAnimation(abajo_arriba);
+
         Thread timerTread = new Thread(){
             public void run(){
                 try{
@@ -27,6 +41,7 @@ public class Splash extends AppCompatActivity {
             }
         };
         timerTread.start();
+
     }
     @Override
     protected void onPause(){
